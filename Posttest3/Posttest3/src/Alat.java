9a9ffac7f8dbcbd3088d2d174541c75e92424cb3
Ahmad Nur Fauzan <113954428+ENDUGI1@users.noreply.github.com>
2024-03-26 00:24:19 +0800

    Update Alat.java

--- Posttest3/Posttest3/src/Alat.java
+++ Posttest3/Posttest3/src/Alat.java
@@ -1,19 +1,9 @@
 public class Alat extends Data{
-    String merkAlat;
     int stokAlat;
 
-    public Alat(String nama, String merkAlat,int harga, int stokAlat) {
+    public Alat(String nama, int harga, int stokAlat) {
         super(nama, harga);
-        this.merkAlat = merkAlat;
         this.stokAlat = stokAlat;
-    }
-
-    public String getMerkAlat() {
-        return merkAlat;
-    }
-
-    public void setMerkAlat(String merkAlat) {
-        this.merkAlat = merkAlat;
     }
 
     public int getStokAlat() {
@@ -26,7 +16,6 @@ public class Alat extends Data{
 
     void tampilAlat() {
         System.out.println("Nama Alat: " + nama);
-        System.out.println("Merk Alat: " + merkAlat);
         System.out.println("Harga Alat: " + harga);
         System.out.println("Stok Alat: " + stokAlat);
     }
